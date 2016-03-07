c0f2480f80eb10893b2519163576d3564fd6b618
Scott Berger <berger17@email.franklin.edu>
2016-03-07 20:05:55 +0000

    Add AngularJS

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -1,12 +1,9 @@
-angular
-    .module('AngularRails', [
-        'ngRoute',
-        'templates'
-    ]).config(function ($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'home.html',
-                controller: 'HomeCtrl'
-            });
-        $locationProvider.html5Mode(true);
-    });
+angular.module('AngularRails', ['ngRoute','templates'])
+  .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+      $routeProvider
+          .when('/', {
+              templateUrl: 'home.html',
+              controller: 'HomeCtrl'
+          });
+      $locationProvider.html5Mode(true);
+  }]);
